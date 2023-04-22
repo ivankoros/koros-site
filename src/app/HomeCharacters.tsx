258bb9a0b9db6aefc79ec967aecb0b831a9f0b9d
ivankoros <ivan.koros2@gmail.com>
2023-04-22 02:11:14 -0400

    added typed props to change between different charactre svg

--- src/app/HomeCharacters.tsx
+++ src/app/HomeCharacters.tsx
@@ -1,17 +1,29 @@
 import Greenguy from '../media/greenguy.svg';
+import Orangeguy from '../media/orangeguy.svg';
 import { motion } from 'framer-motion';
 
-function HomeCharacters() {
+export interface HomeCharactersProps {
+  guy: 'green' | 'orange' | string;
+}
+
+const characterMap: Record<string, string> = {
+  green: Greenguy,
+  orange: Orangeguy,
+};
+
+function HomeCharacters(props: HomeCharactersProps) {
+  const svgToRender = characterMap[props.guy] || Greenguy;
+
   return (
     <motion.div
       whileHover={{
         scale: 1.0,
         translateY: -50,
-        transition: { duration: 0.3 },
+        transition: { duration: 0.2 },
       }}
       className="border border-red-500 w-60 ml-20 pt-20"
     >
-      <img src={Greenguy} alt="greenguy" />
+      <img src={svgToRender} alt="char" />
     </motion.div>
   );
 }
--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,13 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
-import Greenguy from '../media/greenguy.svg';
 import HomeCharacters from "./HomeCharacters";
-import { motion } from "framer-motion";
 
 export function App() {
   return (
-
-      <HomeCharacters />
+      <HomeCharacters guy="Green"/>
   );
 }
 
