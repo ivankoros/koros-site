015bef46d323c1fd24a870e302db689f9eb2f5ac
ivankoros <ivan.koros2@gmail.com>
2023-04-25 16:17:12 -0400

    imported Lato (font) and formatted and spaced text

--- src/app/HomePageText.tsx
+++ src/app/HomePageText.tsx
@@ -1,17 +1,15 @@
-
-
-function HomePageText(){
-    return(
-        <div className="border border-red-500 pt-20">
-            <span className="text-8xl font-Lato"
-            >Hey, my name is </span> <br/>
-            <span className="text-8xl font-Lato font-bold"> Ivan Korostenskij
-            </span>
-            
-            <br/>
-            <span>I'm an aspiring developer</span>
-        </div>
-    )
+function HomePageText() {
+  return (
+    <div className="absolute ml-10 mt-10">
+      <span className="text-[100px] font-Lato">Hey, my name is </span> <br />
+      <span className="text-[100px] font-Lato font-bold">
+        {' '}
+        Ivan Korostenskij
+      </span>
+      <br />
+      <span className="text-[60px] font-lato">I'm an aspiring developer</span>
+    </div>
+  );
 }
 
 export default HomePageText;
