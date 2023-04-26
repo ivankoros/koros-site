d67c336d5410ae2e3b369f6784650ebb6f7490c8
ivankoros <ivan.koros2@gmail.com>
2023-04-26 17:31:56 -0400

    fixed text spacing to be even with social media links

--- src/app/HomePageText.tsx
+++ src/app/HomePageText.tsx
@@ -1,6 +1,6 @@
 function HomePageText() {
   return (
-    <div className="absolute ml-10 mt-10">
+    <div className="absolute ml-10">
       <span className="text-[100px] font-Lato">Hey, my name is </span> <br />
       <span className="text-[100px] font-Lato font-bold">
         {' '}
