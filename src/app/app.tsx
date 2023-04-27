616a4c04101db360988d02b635444b66471e9836
ivankoros <ivan.koros2@gmail.com>
2023-04-26 23:30:44 -0400

    updated app.tsx to used cloud generator function

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -5,11 +5,12 @@ import HomePageText from "./HomePageText";
 import Dock from "./Dock";
 import SocialButtons from './nav_bar/SocialButtons';
 import Clouds from './Clouds';
+import CloudGenerator from './Clouds';
 
 export function App() {
   return (
     <div className='bg-[#A8DADC] w-screen h-screen dark:bg-slate-700'>
-      <Clouds duration={15}/>
+      <CloudGenerator cloud_amount={3}/>
       <SocialButtons/>
       <HomePageText/>
       <Dock/>
