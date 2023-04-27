4764b10384399ec3e09bcaeb5798527312466ec3
ivankoros <ivan.koros2@gmail.com>
2023-04-26 23:07:04 -0400

    added props for frequency and cloud type as props with dictionary mapping

--- src/app/Clouds.tsx
+++ src/app/Clouds.tsx
@@ -1,9 +1,30 @@
 import { motion } from 'framer-motion';
 import { useEffect, useState } from 'react';
 import {ReactComponent as CloudsDuoOne} from '../media/clouds/clouds-duo-1.svg';
+import {ReactComponent as CloudsMogus} from '../media/clouds/clouds-mogus.svg';
+import {ReactComponent as CloudsMultiOne} from '../media/clouds/clouds-multi-1.svg';
+import {ReactComponent as CloudsPlaneOne} from '../media/clouds/clouds-plane-1.svg';
+import {ReactComponent as CloudsTrioOne} from '../media/clouds/clouds-trio-1.svg';
+import {ReactComponent as CloudsTrioTwo} from '../media/clouds/clouds-trio-2.svg';
+
 
 interface CloudsProps {
     duration: number;
+    cloudFrequency: number;
+    cloudType: string;
+}
+
+interface CloudTypes {
+    [key: string]: JSX.Element;
+}
+
+const cloudTypes: CloudTypes = {
+    'duo': <CloudsDuoOne/>,
+    'mogus': <CloudsMogus/>,
+    'multi': <CloudsMultiOne/>,
+    'plane': <CloudsPlaneOne/>,
+    'trio': <CloudsTrioOne/>,
+    'trio2': <CloudsTrioTwo/>,
 }
 
 function Clouds(props: CloudsProps) {
@@ -28,21 +49,26 @@ function Clouds(props: CloudsProps) {
             setCloudStartXPosition(-350);
         }
         window.addEventListener('resize', handleResize);
+
         return () => {
             window.removeEventListener('resize', handleResize);
         }
+
     }, []);
+
+
 
   return (
     <div className="absolute w-screen h-32">
       <motion.div
         style={{ width: 150, x: cloudStartXPosition, y: cloudStartYPosition }}
-      animate={{ x: '100vw' }}
+      animate={{ x: `calc(100vw - ${cloudStartXPosition}px - 150px)` }}
       transition={{ ease: "linear", duration: props.duration, repeat: Infinity }}>
-            <CloudsDuoOne/>
+        {cloudTypes[props.cloudType]}
       </motion.div>
     </div>
   );
 }
+
 
 export default Clouds;
