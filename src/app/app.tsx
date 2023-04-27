eaa5207a61f81a3c953fcac5431f62baee5b3eb5
ivankoros <ivan.koros2@gmail.com>
2023-04-26 22:05:18 -0400

    rendered clouds

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -4,15 +4,17 @@ import Waves from "./Waves";
 import HomePageText from "./HomePageText";
 import Dock from "./Dock";
 import SocialButtons from './nav_bar/SocialButtons';
-
+import Clouds from './Clouds';
 
 export function App() {
   return (
     <div className='bg-[#A8DADC] w-screen h-screen dark:bg-slate-700'>
+      <Clouds duration={15}/>
       <SocialButtons/>
       <HomePageText/>
       <Dock/>
       <Waves/>
+
     </div>
   );
 }
