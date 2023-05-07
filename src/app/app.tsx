427461c2097b21e56b056cdf71937e8ba3a8c491
ivankoros <ivan.koros2@gmail.com>
2023-05-07 19:09:53 -0400

    added stars render

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -5,10 +5,12 @@ import HomePageText from "./HomePageText";
 import Dock from "./Dock";
 import SocialButtons from './nav_bar/SocialButtons';
 import CloudGenerator from './Clouds';
+import Stars from 'src/app/Stars';
 
 export function App() {
   return (
     <div className='bg-[#A8DADC] w-screen h-screen dark:bg-[#141115] transition-all delay-150'>
+      <Stars/>
       <CloudGenerator cloud_amount={8}/>
       <SocialButtons/>
       <HomePageText/>
