9ba1ea56baa75df383987c36c71e9820c5d178e6
Ivan Korostenskij <61260021+ivankoros@users.noreply.github.com>
2023-05-12 23:23:00 -0400

    Update Clouds.tsx

--- src/app/Clouds.tsx
+++ src/app/Clouds.tsx
@@ -20,7 +20,6 @@ interface CloudTypes {
 
 const cloudTypes: CloudTypes = {
   duo: <CloudsDuoOne />,
-  mogus
   multi: <CloudsMultiOne />,
   plane: <CloudsPlaneOne />,
   trio: <CloudsTrioOne />,
